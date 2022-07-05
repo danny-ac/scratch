a934ec03dd726a13d427ba88716e9200ffab2344
Danny Rooney <daniel.rooney@arnoldclark.com>
2022-07-05 12:14:47 +0100

    fixed linting

--- tests/test_add_something.py
+++ tests/test_add_something.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.add_something import AddSomething
 
 
@@ -6,14 +7,9 @@ class TestAddSomething:
     """Test the AddSomething Class."""
 
     @pytest.mark.parametrize(
-        "input_value, expected_value", 
-        [
-            (2,3),
-            (4,5),
-            (-1,0),
-            pytest.param(2,4, marks=pytest.mark.xfail)
-        ]
-        )
+        "input_value, expected_value",
+        [(2, 3), (4, 5), (-1, 0), pytest.param(2, 4, marks=pytest.mark.xfail)],
+    )
     def test_add_one_on(self, input_value, expected_value):
         actual_value = AddSomething.add_one_on(input_value)
         assert expected_value == actual_value
