4c7ef1da402fa90392989e1c4c586955f93f9815
Danny Rooney <daniel.rooney@arnoldclark.com>
2022-07-05 12:09:56 +0100

    cleaned up test file

--- tests/test_add_something.py
+++ tests/test_add_something.py
@@ -1,18 +1,19 @@
-from sre_constants import ASSERT_NOT
+import pytest
 from src.add_something import AddSomething
 
 
 class TestAddSomething:
     """Test the AddSomething Class."""
 
-    def test_add_one_on(self):
-        input_value = 2
-        expected_value = 3
+    @pytest.mark.parametrize(
+        "input_value, expected_value", 
+        [
+            (2,3),
+            (4,5),
+            (-1,0),
+            pytest.param(2,4, marks=pytest.mark.xfail)
+        ]
+        )
+    def test_add_one_on(self, input_value, expected_value):
         actual_value = AddSomething.add_one_on(input_value)
         assert expected_value == actual_value
-    
-    def test_add_one_onto_2_is_not_4(self):
-        input_value = 2
-        expected_value = 4
-        actual_value = AddSomething.add_one_on(input_value)
-        assert expected_value != actual_value
